3b5722612cb3084c364cd61d4c5f25f171f4fe0f
Kobu <thekobu@gmail.com>
2023-06-24 20:31:23 +0200

    refactor: add explicity model binding

--- Salutaris.Api/Contracts/Requests/GetGroupByIdRequest.cs
+++ Salutaris.Api/Contracts/Requests/GetGroupByIdRequest.cs
@@ -8,6 +8,7 @@ namespace salutaris.Contracts.Requests;
 
 public class GetGroupByIdRequest
 {
+    [BindFrom("id")]
     public required Guid Id { get; init; }
     [FromClaim] public Guid UserId { get; init; } = default;
 }
